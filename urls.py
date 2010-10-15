5ef99bb099e9380a26b0b8589f7d8bfe317d4188
Vanderson Mota <vanderson.mota@myfreecomm.com.br>
2010-10-14 21:39:45 -0300

    adiciona url para servir arquivos estáticos

--- urls.py
+++ urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 
 urlpatterns = patterns('',
     url(r'', include('youtube_search.urls')),
 )
+
+urlpatterns += patterns('',
+    (r'^/media/(?P<path>.*)$', 'django.views.static.serve',
+        { 'document_root': settings.MEDIA_ROOT }),
+    )
