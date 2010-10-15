4fd092d2797f78b326b74aab7ae294152b0c465b
Thiago Garcia <thiagogds14@gmail.com>
2010-10-14 22:02:30 -0300

    Adiciona configuração para conseguir achar o media

--- urls.py
+++ urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
 )
 
 urlpatterns += patterns('',
-    (r'^/media/(?P<path>.*)$', 'django.views.static.serve',
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve',
         { 'document_root': settings.MEDIA_ROOT }),
-    )
+)
